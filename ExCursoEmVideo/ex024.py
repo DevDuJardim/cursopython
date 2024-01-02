b44e2787d166d68377ecd67de70fb90081c15d9f
diego do <>
2024-01-02 20:26:02 -0300

    Update ex024.py

--- ExCursoEmVideo/ex024.py
+++ ExCursoEmVideo/ex024.py
@@ -1,3 +1,13 @@
 '''Faça um algoritmo que pergunte a distância que um passageiro deseja
 percorrer em Km. Calcule o preço da passagem, cobrando R$0.50 por Km para
 viagens até 200Km e R$0.45 para viagens mais longas.'''
+print('         PASSAGEM\n         ')
+print('''---------------------------------------------
+        Calcule o preço da passagem:''')
+dis_km = float(input('\nDistancia em Km: '))
+menor200 = dis_km * 0.5
+maior200 = dis_km * 0.45
+if dis_km <= 200:
+    print('O valor da passagem será R$ {:.2f}.'.format(menor200))
+else:
+    print('O valor da passagem será R$ {:.2f}'.format(maior200))
