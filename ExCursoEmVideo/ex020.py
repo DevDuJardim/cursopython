743037d6f5c306840d76ae010b04dd7f22a395a7
diego do <>
2024-01-02 16:45:29 -0300

    exercicio

--- ExCursoEmVideo/ex020.py
+++ ExCursoEmVideo/ex020.py
@@ -1,2 +1,9 @@
 '''Desenvolva um programa que leia um número inteiro e mostre se ele é PAR ou
 ÍMPAR.'''
+print('      PAR OU IMPAR\n      ')
+
+num = int(input('Digite um numero inteiro:'))
+if num % 2 == 0:
+    print('O numero {} é um numero par.'.format(num))
+else:
+    print('O numero {} é um numero impar'.format(num))
