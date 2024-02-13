76d5109038cc02ce647c4c26402d9f384dcffda6
diego do <>
2024-02-13 18:49:57 -0300

    ddd

--- ExCursoEmVideo/ex028.py
+++ ExCursoEmVideo/ex028.py
@@ -3,3 +3,4 @@ retangular, calculando e mostrando a sua área em m². O programa também
 devemostrar a classificação desse terreno, de acordo com a lista abaixo:
 - Abaixo de 100m² = TERRENO POPULAR
 - Entre 100m² e 500m² = TERRENO'''
+print ('repositorio')
